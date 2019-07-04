bc9d4afc3430d444aad00e3314b707566c18db56
Michal.Polinkiewicz <Michal.Polinkiewicz@empirica.pl>
2019-07-04 10:58:53 +0200

    modify html-link-parser

--- html-link-parser/main.go
+++ html-link-parser/main.go
@@ -15,37 +15,32 @@ type link struct {
 }
 
 func main() {
-
-	s := `<p>Links:</p>
+	pageMock := `<p>Links:</p>
 			<ul>
 			<li>
 			<a href="foo">Foo</a><li>
 			<a href="/bar/baz">BarBaz</a>
 			</ul>
-			<a href="/janusz">aaa</a>
-`
+			<a href="/janusz">aaa</a>`
 
-	doc, err := html.Parse(strings.NewReader(s))
+	doc, err := html.Parse(strings.NewReader(pageMock))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var f func(*html.Node)
-	f = func(n *html.Node) {
-		if n.Data == "a" {
-			for _, a := range n.Attr {
-				if a.Key == "href" {
-					links = append(links, link{a.Val, n.FirstChild.Data})
-				}
-			}
-		}
-		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			f(c)
-		}
-	}
-	f(doc)
-
+	getLinks(doc)
 	fmt.Println(links)
-	
 }
 
+func getLinks(n *html.Node) {
+	if n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				links = append(links, link{a.Val, n.FirstChild.Data})
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		getLinks(c)
+	}
+}
