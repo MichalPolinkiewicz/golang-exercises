e7ffaf0c801281f9b59f0659d5e102a9239d127d
Michal.Polinkiewicz <Michal.Polinkiewicz@empirica.pl>
2019-07-26 11:24:06 +0200

    add temperature converter

--- temp-conv/model.go
+++ temp-conv/model.go
@@ -2,16 +2,6 @@ package temp_conv
 
 import "fmt"
 
-const (
-	AbsoluteZeroC Celsius = -273.15
-	FreezingC Celsius = 0
-	BoilingC Celsius = 100
-
-
-	FreezingK = 273.15
-
-)
-
 type Celsius float64
 type Fahrenheit float64
 type Kelvin float64
